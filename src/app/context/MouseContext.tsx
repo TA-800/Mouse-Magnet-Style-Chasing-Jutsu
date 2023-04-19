e388853b8b49b921a3ab5c0a5f40d28d6b6f6eb6
TA-800 <taherali0905@gmail.com>
2023-04-19 03:05:58 -0400

    build 3.5 - useContext

--- src/app/context/MouseContext.tsx
+++ src/app/context/MouseContext.tsx
@@ -1,15 +1,28 @@
-// import React, { useRef } from "react";
+import { useRef, createContext } from "react";
 
-// export const MouseContext = React.createContext({
-//     isHovering: false,
-//     targetMousePosition: { x: 0, y: 0 },
-//     mouseRef: null,
-// });
+export interface MouseContextType {
+    isHovering: React.MutableRefObject<boolean>;
+    targetMousePosition: { x: number; y: number };
+    mouseRef: React.MutableRefObject<HTMLDivElement | null>;
+}
 
-// export const MouseProvider = ({ children }: { children: React.ReactNode }) => {
-//     const targetMousePosition = { x: 0, y: 0 };
-//     const isHovering = useRef(false);
-//     const mouseRef = useRef<HTMLDivElement>(null);
+// default value for context
+export const MouseContext = createContext<MouseContextType>({
+    isHovering: { current: false },
+    targetMousePosition: { x: 0, y: 0 },
+    mouseRef: { current: null },
+});
 
-//     return <MouseContext.Provider value={{ isHovering, targetMousePosition, mouseRef }}>{children}</MouseContext.Provider>;
-// };
+export const MouseProvider = ({ children }: { children: React.ReactNode }) => {
+    const targetMousePosition = { x: 0, y: 0 };
+    const isHovering = useRef(false);
+    const mouseRef = useRef<HTMLDivElement>(null);
+
+    const context = {
+        isHovering,
+        targetMousePosition,
+        mouseRef,
+    };
+
+    return <MouseContext.Provider value={context}>{children}</MouseContext.Provider>;
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,136 +1,114 @@
 "use client";
 
-import { forwardRef, useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState, useContext } from "react";
+import { MouseContext, MouseProvider } from "./context/MouseContext";
 
 // reference: https://frontendmasters.com/courses/css-animations/lerp-technique/
 export default function Home() {
-    const targetMousePosition = { x: 0, y: 0 };
-    const isHovering = useRef(false);
-    const mouseRef = useRef<HTMLDivElement>(null);
-
     return (
-        <main className="flex flex-row gap-10 justify-center items-center mt-20">
-            <MouseFollower isHovering={isHovering} targetMousePosition={targetMousePosition} mouseRef={mouseRef} />
-            <MagneticButton isHovering={isHovering} targetMousePosition={targetMousePosition} mouseRef={mouseRef}>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                </svg>
-            </MagneticButton>
-        </main>
+        <MouseProvider>
+            <main className="flex flex-row gap-10 justify-center items-center mt-20">
+                <MouseFollower />
+                <MagneticButton>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                </MagneticButton>
+            </main>
+        </MouseProvider>
     );
 }
 
-const MouseFollower = forwardRef(
-    ({
-        isHovering,
-        targetMousePosition,
-        mouseRef,
-    }: {
-        isHovering: React.MutableRefObject<boolean>;
-        targetMousePosition: { x: number; y: number };
-        mouseRef: React.MutableRefObject<HTMLDivElement | null>;
-    }) => {
-        const MOUSE_SPEED = 0.1; // how fast the circle follows the mouse
-        const currentMousePosition = { x: targetMousePosition.x, y: targetMousePosition.y }; // {x: 0, y: 0};
-        const [isHoveringState, setIsHoveringState] = useState(isHovering.current);
-        // if animationID is null, then the animation loop is not running
-        let mouseAnimationID: number | null = null;
+const MouseFollower = () => {
+    const MOUSE_SPEED = 0.1; // how fast the circle follows the mouse
+    const { isHovering, targetMousePosition, mouseRef } = useContext(MouseContext);
+    const currentMousePosition = { x: targetMousePosition.x, y: targetMousePosition.y }; // {x: 0, y: 0};
+    const [isHoveringState, setIsHoveringState] = useState(isHovering.current);
+    let mouseAnimationID: number | null = null; // if animationID is null, then the animation loop is not running
 
-        // applying new targetMousePosition to currentMousePosition
-        const mouseLerp = useCallback(() => {
-            const dx = targetMousePosition.x - currentMousePosition.x;
-            const dy = targetMousePosition.y - currentMousePosition.y;
+    // applying new targetMousePosition to currentMousePosition
+    const mouseLerp = useCallback(() => {
+        const dx = targetMousePosition.x - currentMousePosition.x;
+        const dy = targetMousePosition.y - currentMousePosition.y;
 
-            // If animation is running and distance is less than 0.01, stop the animation
-            if (mouseAnimationID && Math.abs(dx) < 0.01 && Math.abs(dy) < 0.01) {
-                cancelAnimationFrame(mouseAnimationID);
-                mouseAnimationID = null;
-                return;
-            }
+        // If animation is running and distance is less than 0.01, stop the animation
+        if (mouseAnimationID && Math.abs(dx) < 0.01 && Math.abs(dy) < 0.01) {
+            cancelAnimationFrame(mouseAnimationID);
+            mouseAnimationID = null;
+            return;
+        }
 
-            currentMousePosition.x += dx * MOUSE_SPEED;
-            currentMousePosition.y += dy * MOUSE_SPEED;
+        currentMousePosition.x += dx * MOUSE_SPEED;
+        currentMousePosition.y += dy * MOUSE_SPEED;
 
-            // set translateX and translateY CSS variables to the normalized values
-            if (mouseRef.current) {
-                mouseRef.current.style.setProperty("--mouseX", currentMousePosition.x + "px");
-                mouseRef.current.style.setProperty("--mouseY", currentMousePosition.y + "px");
-            }
+        // set translateX and translateY CSS variables to the normalized values
+        if (mouseRef.current) {
+            mouseRef.current.style.setProperty("--mouseX", currentMousePosition.x + "px");
+            mouseRef.current.style.setProperty("--mouseY", currentMousePosition.y + "px");
+        }
 
-            // keep the animation loop running
-            mouseAnimationID = requestAnimationFrame(mouseLerp);
-        }, []);
+        // keep the animation loop running
+        mouseAnimationID = requestAnimationFrame(mouseLerp);
+    }, []);
 
-        // updating targetMousePosition and then calling mouseLerp
-        const handleMouseMove = useCallback((e: MouseEvent) => {
-            // get the x and y coordinates of the mouse
-            const x = e.clientX;
-            const y = e.clientY;
+    // updating targetMousePosition and then calling mouseLerp
+    const handleMouseMove = useCallback((e: MouseEvent) => {
+        // get the x and y coordinates of the mouse
+        const x = e.clientX;
+        const y = e.clientY;
 
-            // get height / 2 and width / 2 of the mouse follower circle
-            const middleX = mouseRef.current!.offsetWidth / 2 || 0;
-            const middleY = mouseRef.current!.offsetHeight / 2 || 0;
+        // get height / 2 and width / 2 of the mouse follower circle
+        const middleX = mouseRef.current!.offsetWidth / 2 || 0;
+        const middleY = mouseRef.current!.offsetHeight / 2 || 0;
 
-            // get target position (no need to normalize)
-            if (!isHovering.current) {
-                // only update targetMousePosition if the mouse is not hovering over a magnetic button
-                targetMousePosition.x = x - middleX;
-                targetMousePosition.y = y - middleY;
-                setIsHoveringState(false); // React will not re-render if the state is the same
-            } else {
-                setIsHoveringState(true);
-            }
+        // get target position (no need to normalize)
+        if (!isHovering.current) {
+            // only update targetMousePosition if the mouse is not hovering over a magnetic button
+            targetMousePosition.x = x - middleX;
+            targetMousePosition.y = y - middleY;
+            setIsHoveringState(false); // React will not re-render if the state is the same
+        } else {
+            setIsHoveringState(true);
+        }
 
-            // start animation loop if it is not running
-            if (!mouseAnimationID) requestAnimationFrame(mouseLerp);
-        }, []);
+        // start animation loop if it is not running
+        if (!mouseAnimationID) requestAnimationFrame(mouseLerp);
+    }, []);
 
-        useEffect(() => {
-            document.addEventListener("mousemove", handleMouseMove);
-            return () => {
-                document.removeEventListener("mousemove", handleMouseMove);
-            };
-        }, []);
+    useEffect(() => {
+        document.addEventListener("mousemove", handleMouseMove);
+        return () => {
+            document.removeEventListener("mousemove", handleMouseMove);
+        };
+    }, []);
 
-        return (
-            <div
-                id="mouse-follower"
-                ref={mouseRef}
-                className={`absolute -z-50 h-3 w-3 rounded-full bg-white top-0 left-0 translate-x-[var(--mouseX)] translate-y-[var(--mouseY)]
+    return (
+        <div
+            ref={mouseRef}
+            className={`absolute -z-50 h-3 w-3 rounded-full bg-white top-0 left-0 translate-x-[var(--mouseX)] translate-y-[var(--mouseY)]
                         ${isHoveringState ? `scale-[5]` : "scale-[1]"}
             `}
-                style={{
-                    transition: "scale 1s ease-in-out",
-                }}
-            />
-        );
-    }
-);
+            style={{
+                transition: "scale 1s ease-in-out",
+            }}
+        />
+    );
+};
 
-function MagneticButton({
-    isHovering,
-    targetMousePosition,
-    mouseRef,
-    children,
-}: {
-    isHovering: React.MutableRefObject<boolean>;
-    targetMousePosition: { x: number; y: number };
-    mouseRef: React.MutableRefObject<HTMLDivElement | null>;
-    children: React.ReactNode;
-}) {
+function MagneticButton({ children }: { children: React.ReactNode }) {
     const ICON_OFFSET = 20; // how much the icon moves when the mouse is hovering over it
     const ICON_SCALE = "1.15"; // how much the icon scales when the mouse is hovering over it
     const ICON_SPEED = 0.1; // how fast the icon moves
+    const { isHovering, targetMousePosition, mouseRef } = useContext(MouseContext);
     const currentPoint = { x: 0, y: 0 };
     const targetPoint = { x: 0, y: 0 };
-    // if animationID is null, then the animation loop is not running
-    let animationID: number | null = null;
+    let animationID: number | null = null; // if animationID is null, then the animation loop is not running
     const childRef = useRef<HTMLDivElement>(null);
 
     // useCallback to prevent unnecessary redefinition of lerp function (prevents glitchy animation on state change)
